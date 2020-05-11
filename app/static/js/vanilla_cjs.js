68c0818ddd10bb66136a9c715e29178cbc74df13
Ben Foltz <ben.p.foltz@gmail.com>
2020-05-11 15:29:21 -0400

    slider test & testimonial

--- app/static/js/vanilla_cjs.js
+++ app/static/js/vanilla_cjs.js
@@ -28,6 +28,8 @@ function showSlides(n) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
 
+    console.log(slides[slideIndex-1]);
+    
     slides[slideIndex-1].style.display = "block"; 
     dots[slideIndex-1].className += " active";
 }
